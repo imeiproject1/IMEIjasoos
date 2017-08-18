646e45ae55dd38633041e7e6708135a03cdbac42
arpitbajaj <arpitbajaj95@gmail.com>
2017-08-19 00:09:55 +0530

    Added Splash Screem

--- app/src/main/java/com/example/android/imeijasoos/MainActivity.java
+++ app/src/main/java/com/example/android/imeijasoos/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.android.imeijasoos;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_TINE_OUT=2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent HomeIntent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(HomeIntent);
+                finish();
+            }
+        },SPLASH_TINE_OUT);
     }
 }
