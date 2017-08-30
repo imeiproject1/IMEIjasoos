c1b9328b115ef8b2a04d925e5dae729e3ea4893b
arpitbajaj <arpitbajaj95@gmail.com>
2017-08-30 18:13:46 +0530

    IMEI check logic updated

--- app/src/main/java/com/example/android/imeijasoos/HomeActivity.java
+++ app/src/main/java/com/example/android/imeijasoos/HomeActivity.java
@@ -28,6 +28,7 @@ import java.net.URL;
 import java.net.URLEncoder;
 import java.util.concurrent.ExecutionException;
 
+import static java.sql.Types.INTEGER;
 import static java.sql.Types.NULL;
 
 
@@ -40,12 +41,12 @@ public class HomeActivity extends AppCompatActivity {
         IMEIEntered = (EditText)findViewById(R.id.etIMEI);
     }
     public void OnButtonClick(View view) throws ExecutionException, InterruptedException {
-        String username = IMEIEntered.getText().toString();
-        username = username.substring(0,8);
+        String imei_number = IMEIEntered.getText().toString();
+        String tac_number = imei_number.substring(0,8);
         String password = "";
         String type = "login";
         BackgroundWorker backgroundWorker = new BackgroundWorker(this,HomeActivity.this);
-        backgroundWorker.execute(type,username,password);
+        backgroundWorker.execute(type,imei_number,tac_number,password);
     }
     public class BackgroundWorker extends AsyncTask<String,Void,String> {
         Context context;
@@ -57,12 +58,14 @@ public class HomeActivity extends AppCompatActivity {
         }
         @Override
         protected String doInBackground(String... params) {
+            String retval = "";
             String type = params[0];
-            String login_url = "http://192.168.110.60/IMEIjasoos/login.php";
+            String login_url = "http://192.168.110.167/IMEIjasoos/login.php";
             if(type.equals("login")) {
                 try {
 
-                    String user_name = params[1];
+                    String imei_code = params[1];
+                    String tac_code = params[2];
                     //String password = params[2];
                     URL url = new URL(login_url);
                     Log.d("mydebug","Working till here");
@@ -73,7 +76,7 @@ public class HomeActivity extends AppCompatActivity {
                     httpURLConnection.setDoInput(true);
                     OutputStream outputStream = httpURLConnection.getOutputStream();
                     BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
-                    String post_data = URLEncoder.encode("imei_n","UTF-8")+"="+URLEncoder.encode(user_name,"UTF-8");
+                    String post_data = URLEncoder.encode("imei_n","UTF-8")+"="+URLEncoder.encode(imei_code,"UTF-8")+"&"+URLEncoder.encode("tac_n","UTF-8")+"="+URLEncoder.encode(tac_code,"UTF-8");
                     bufferedWriter.write(post_data);
                     bufferedWriter.flush();
                     bufferedWriter.close();
@@ -88,14 +91,49 @@ public class HomeActivity extends AppCompatActivity {
                     bufferedReader.close();
                     inputStream.close();
                     httpURLConnection.disconnect();
+                    if(result.equals("IMEI is valid")){
+                        int sumval=0;
+                        String even_nu="";
+                        for(int i = 0; i<14;i++){
+                            //Log.d("mydebug", String.valueOf(imei_code.charAt(i)));
+                            if(i%2==0)
+                                sumval += Character.getNumericValue(imei_code.charAt(i));
+                            else {
+                                int temp = Character.getNumericValue(imei_code.charAt(i)) * 2;
+                                even_nu += temp;
+                            }
+                        }
+                        Log.d("mydebug",Integer.toString(sumval));
+                        Log.d("mydebug",even_nu);
+                        for(int i = 0; i<even_nu.length(); i++) {
+                            sumval += (Character.getNumericValue(even_nu.charAt(i)));
+                        }
+                        Log.d("mydebug",Integer.toString(sumval));
+                        int t2 = sumval%10;
+                        if(t2>0)
+                            t2=10-t2;
+                      //  Log.d("mydebug",Integer.toString(t2));
+
+                        //Log.d("mydebug",Integer.toString(Character.getNumericValue(imei_code.charAt(14))));
+
+                        if(t2==Character.getNumericValue(imei_code.charAt(14)))
+                            result = "IMEI is Valid";
+                        else
+                            result = "IMEI is Invalid";
+
+
+
+
+                    }
+
                     return result;
+
                 } catch (MalformedURLException e) {
                     e.printStackTrace();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
-            String retval = "";
             return retval;
         }
 
@@ -107,6 +145,7 @@ public class HomeActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(String result) {
+
 
             alertDialog.setMessage(result);
             alertDialog.show();
